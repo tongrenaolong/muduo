7ecb4f3c68aa23519c1189f6cd217b2f73dd8cdb
tongrenaolong <3300763927@qq.com>
2025-07-05 11:34:13 +0800

    fix ZlibStream_unittest.cc

--- muduo/net/tests/ZlibStream_unittest.cc
+++ muduo/net/tests/ZlibStream_unittest.cc
@@ -89,3 +89,56 @@ BOOST_AUTO_TEST_CASE(testZlibOutputStream5)
   printf("total %zd\n", output.readableBytes());
   BOOST_CHECK_EQUAL(stream.zlibErrorCode(), Z_STREAM_END);
 }
+
+
+BOOST_AUTO_TEST_CASE(testZlibInputStream) {
+  muduo::net::Buffer input;
+  muduo::net::Buffer output;
+  {
+    muduo::net::ZlibOutputStream outStream(&input);
+    outStream.write("Hello, Zlib!");
+    outStream.finish();
+  }
+
+  muduo::net::ZlibInputStream inStream(&input);
+  std::string result;
+  while (inStream.zlibErrorCode() == Z_OK) {
+    muduo::net::Buffer temp;
+    if (inStream.read(&temp)) {
+      result.append(temp.peek(), temp.readableBytes());
+    }
+  }
+  BOOST_CHECK_EQUAL(result, "Hello, Zlib!");
+}
+
+BOOST_AUTO_TEST_CASE(testZlibInputStreamEmpty) {
+  muduo::net::Buffer input;
+  muduo::net::ZlibInputStream inStream(&input);
+  muduo::net::Buffer output;
+  BOOST_CHECK(!inStream.read(&output));
+  BOOST_CHECK_EQUAL(output.readableBytes(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(testZlibInputStreamLargeData) {
+  muduo::net::Buffer input;
+  std::string largeData;
+  for (int i = 0; i < 1000; ++i) {
+    largeData += "This is a test for large data compression and decompression. ";
+  }
+  
+  {
+    muduo::net::ZlibOutputStream outStream(&input);
+    outStream.write(largeData);
+    outStream.finish();
+  }
+
+  muduo::net::ZlibInputStream inStream(&input);
+  std::string result;
+  while (inStream.zlibErrorCode() == Z_OK) {
+    muduo::net::Buffer temp;
+    if (inStream.read(&temp)) {
+      result.append(temp.peek(), temp.readableBytes());
+    }
+  }
+  BOOST_CHECK_EQUAL(result, largeData);
+}
